ad03c6030563d35c651df6673ef38d3687789c56
thanhtaita <taita.studymail@gmail.com>
2023-09-23 17:45:17 -0500

    add excalidraw link and MVP description

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <h1 className="text-3xl font-bold underline">
         Welcome to Geo Scavenger Hunt - Your Ultimate Adventure Awaits!🌍🔍🏞️
       </h1>
+
+      <a href="https://www.figma.com/file/ZhlbOvlkBbcFFk4VikCMqq/cs484-checkpoint-1?type=whiteboard&node-id=0-1&t=vqTg9O9zLMPmHQJy-0">
+        <h3>Excalidraw Diagram</h3>
+      </a>
+
+      <p>
+        The MVP for this project entails setting up OAuth-based authentication
+        and authorization for users, creating React front-end interfaces
+        (including login pages) for admin and player views, establishing an
+        Express server with PostgreSQL databases, and implementing core API
+        communication between the views and the server. Integration of external
+        APIs like Google Maps and QR scanning APIs will enhance functionality.
+        The development of server-side code will accommodate these features and
+        ensure seamless operation, serving both admin and player needs.
+      </p>
+
       {/* <div className="bg-blue-500 text-white p-4">
         <p className="text-xl font-semibold">Get ready to embark on an exciting journey of discovery and exploration. Whether you're a seasoned adventurer or new to the world of scavenger hunts, our app is your passport to a world of fun, challenges, and hidden treasures. Grab your smartphone, gather your friends, and let the adventure begin! Navigate through intriguing clues, explore fascinating locations, and uncover the secrets of the world around you.</p>
         <p className="mt-2">This is a sample product description.</p>
@@ -21,7 +32,7 @@ function App() {
         <p className="mt-2">Another product with a description.</p>
       </div> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
